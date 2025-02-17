4a23ab0c50e030598c55c2382c358e4a161f1b05
Exter-dg <prkandmap23@rocketmail.com>
2025-02-17 10:25:49 +0000

    Implemented CI pipeline

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,15 +1,19 @@
 /// <reference types='vite/client' />
+/// <reference types='vitest' />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/my-app',
   server: {
     host: true,
     port: 8081,
-    allowedHosts: ["d38oyzxuphfai.cloudfront.net"]
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },  
 })
